505895b03d0d893e550cf402726693ea889a62a7
User <User@DESKTOP-9ODT9HC.mshome.net>
2024-07-30 15:48:14 +0530

    first push

---
+++ Selenium_project/src/test/java/pac1/OrgTest.java
@@ -0,0 +1,17 @@
+package pac1;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	@Test
+	public void createcontactTest()
+	{
+		System.out.println("execute createcontactTest");
+	}
+	@Test
+	public void modifycontactTest()
+	{
+		System.out.println("execute modifycontactTest");
+	}
+
+}
---
+++ Selenium_project/src/test/java/pac2/ContactTest.java
@@ -0,0 +1,17 @@
+package pac2;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	@Test
+	public void createcontactTest()
+	{
+		System.out.println("execute createcontactTest");
+	}
+	@Test
+	public void modifycontactTest()
+	{
+		System.out.println("execute modifycontactTest");
+	}
+
+}

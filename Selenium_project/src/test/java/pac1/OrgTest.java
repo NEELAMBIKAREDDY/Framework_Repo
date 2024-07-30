9270992ff79ef3209dc8bd70055365249b3d9936
User <User@DESKTOP-9ODT9HC.mshome.net>
2024-07-30 16:43:52 +0530

    smoke and regression xmlfile is added

--- Selenium_project/src/test/java/pac1/OrgTest.java
+++ Selenium_project/src/test/java/pac1/OrgTest.java
@@ -7,6 +7,15 @@ public class OrgTest {
 	public void createcontactTest()
 	{
 		System.out.println("execute createcontactTest");
+		String URL = System.getProperty("url");
+		String BROWSER = System.getProperty("Browser");
+		String USERNAME = System.getProperty("username");
+		String PASSWORD = System.getProperty("password");
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+		
 	}
 	@Test
 	public void modifycontactTest()
--- Selenium_project/src/test/java/pac2/ContactTest.java
+++ Selenium_project/src/test/java/pac2/ContactTest.java
@@ -7,6 +7,14 @@ public class ContactTest {
 	public void createcontactTest()
 	{
 		System.out.println("execute createcontactTest");
+		String URL = System.getProperty("url");
+		String BROWSER = System.getProperty("Browser");
+		String USERNAME = System.getProperty("username");
+		String PASSWORD = System.getProperty("password");
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
 	}
 	@Test
 	public void modifycontactTest()
